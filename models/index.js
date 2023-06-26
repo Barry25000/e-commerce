73422f008ee91dc848489027411c833290ff55b0
Barry25000 <barryengler@gmail.com>
2023-06-26 16:29:18 -0500

    model update

--- models/index.js
+++ models/index.js
@@ -19,13 +19,19 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
-  forignKey: "product_tag_id",
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
 });
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-  forignKey: "tag_id",
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
 });
 
 module.exports = {
